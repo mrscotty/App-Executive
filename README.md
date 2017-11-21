eb180bfddcda2fa64a7142eefeae1b1e1bfa3fdf
Scott Hardin <scott@hnsc.de>
2017-11-21 13:08:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Executive - It's new $module
+
+# SYNOPSIS
+
+    use App::Executive;
+
+# DESCRIPTION
+
+App::Executive is ...
+
+# LICENSE
+
+Copyright (C) Scott Hardin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Scott Hardin <scott@hnsc.de>
---
+++ lib/App/Executive.pm
@@ -0,0 +1,39 @@
+package App::Executive;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Executive - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Executive;
+
+=head1 DESCRIPTION
+
+App::Executive is ...
+
+=head1 LICENSE
+
+Copyright (C) Scott Hardin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Scott Hardin E<lt>scott@hnsc.deE<gt>
+
+=cut
+
